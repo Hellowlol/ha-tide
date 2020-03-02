f5a760984a08a3c3e2bbfa7ef5140f68a39f5bf4
Hellowlol <Hellowlol@users.noreply.github.com>
2020-03-02 22:41:13 +0100

    Update __init__.py
    ran black and more config flow stuff

--- custom_components/tide/__init__.py
+++ custom_components/tide/__init__.py
@@ -1,15 +1,15 @@
 import logging
 
 from homeassistant import config_entries
-
+from homeassistant.core import Config, HomeAssistant
 
 DOMAIN = "tide"
 
 _LOGGER = logging.getLogger(__name__)
 
 NAME = DOMAIN
-VERSION = '0.0.1'
-ISSUEURL = 'https://github.com/hellowlol/ha-tide/issues'
+VERSION = "0.0.1"
+ISSUEURL = "https://github.com/hellowlol/ha-tide/issues"
 
 STARTUP = """
 -------------------------------------------------------------------
@@ -19,10 +19,12 @@ This is a custom component
 If you have any issues with this you need to open an issue here:
 {issueurl}
 -------------------------------------------------------------------
-""".format(name=NAME, version=VERSION, issueurl=ISSUEURL)
+""".format(
+    name=NAME, version=VERSION, issueurl=ISSUEURL
+)
 
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: Config) -> bool:
     """Set up this component using YAML."""
     _LOGGER.info(STARTUP)
     if config.get(DOMAIN) is None:
@@ -31,7 +33,6 @@ async def async_setup(hass, config):
 
     try:
         await hass.config_entries.async_forward_entry(config, "sensor")
-        _LOGGER.info("Successfully added sensor from the blueprint integration")
     except ValueError:
         pass
 
@@ -40,4 +41,21 @@ async def async_setup(hass, config):
             DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data={}
         )
     )
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: config_entries.ConfigEntry
+) -> bool:
+    """Set up tide as config entry."""
+
+    hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant, entry: config_entries.ConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return True
